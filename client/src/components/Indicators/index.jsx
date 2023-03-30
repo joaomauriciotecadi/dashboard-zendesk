6d8a2aa955bed3aa150101407bc90b94bc364b2d
joaomauriciotecadi <125367763+joaomauriciotecadi@users.noreply.github.com>
2023-03-30 14:58:37 -0300

    Update index.jsx

--- client/src/components/Indicators/index.jsx
+++ client/src/components/Indicators/index.jsx
@@ -10,34 +10,24 @@ import {
 import { priority } from '../../utils/utils'
 
 const Indicators = ({ listTickets }) => {
-  const stats = [{ crit: 'Urgent', color: 'red' },
-  { crit: 'High', color: 'orange' }, { crit: 'Normal', color: 'yellow' }, { crit: 'Low', color: 'red' }]
+  const stats = [{ crit: 'Tickets', color: '' }, { crit: 'Urgent', color: 'red' },
+  { crit: 'High', color: 'orange' }, { crit: 'Normal', color: 'yellow' }, { crit: 'Low', color: 'green' }]
 
 
   return (
     <Flex justify={'space-around'} mt='2'>
-      <Card minW='48'>
-        <CardHeader>
-          <Heading size='sm'>Tickets</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>{listTickets ? listTickets.length : <Spinner
-            thickness='2px'
-            speed='0.65s'
-            emptyColor='gray.200'
-            color='blue.500'
-            size='sm'
-          />}</Text>
-        </CardBody>
-      </Card>
       {
         stats?.map((stat) => {
-          return (<Card colorScheme={'red'} minW='48'>
-            <CardHeader>
-              <Heading size='sm'>{stat.crit}</Heading>
-            </CardHeader>
+          return (<Card bg={"gray.700"} color={stat.color} minW='48'>
             <CardBody>
-              <Text>8</Text>
+              <Heading size='sm' color={stat.color}>{stat.crit}</Heading>
+              <Text fontSize={'7xl'} align={'center'}  >{listTickets ? listTickets.length : <Spinner
+                thickness='2px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='sm'
+              />}</Text>
             </CardBody>
           </Card>)
         })
